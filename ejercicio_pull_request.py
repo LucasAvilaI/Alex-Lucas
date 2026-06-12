d31d278ab860545a355b535db7e7d5cc235b5c10
alex-bometon <alex_bometon@alexbometon.com>
2026-06-12 13:04:12 +0200

    chore: creada funcion media_notas

--- ejercicio_pull_request.py
+++ ejercicio_pull_request.py
@@ -4,4 +4,15 @@ nota = [1,2,3,4]
 
 for x,y in enumerate(nota):
     print(x,y)
-    
+
+def media_notas(notas: list) -> float:
+    """
+    Recibe las notas en una lista.
+    Calcula la media y devuelve el resultado en un float.
+    """
+
+    return sum(notas)/len(notas)
+
+nota_media = media_notas(nota)
+
+print(f"La media de las notas es: {nota_media}")
